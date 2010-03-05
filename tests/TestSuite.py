e4c013246a952dc0b8fcde5ae8bf67abb398aba0
Yoann Sababady <yoann@iMac-de-Yoann-Sababady.local>
2010-03-05 16:58:47 +0100

    replacing tests/test_update_dynamic.rst with tests/UpdateDynamicTests.py, updating tests/TestSuite.py

--- tests/TestSuite.py
+++ tests/TestSuite.py
@@ -3,8 +3,10 @@
 
 import unittest
 import JointsTests, ConstraintsTests, HomogeneousMatrixTests, Human36Tests
+import UpdateDynamicTests
 
 tests = unittest.TestSuite([JointsTests.ts, ConstraintsTests.ts,
-                            HomogeneousMatrixTests.ts, Human36Tests.ts])
+                            HomogeneousMatrixTests.ts, Human36Tests.ts,
+                            UpdateDynamicTests.ts])
 
 unittest.TextTestRunner(verbosity=2).run(tests)
