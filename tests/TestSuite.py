cc74469b2cc0265dbcde161850dcb5cfff7bca48
Yoann Sababady <yoann@iMac-de-Yoann-Sababady.local>
2010-03-05 16:47:52 +0100

    replacing tests/test_human36.rst  with tests/Human36Tests.py, updating tests/TestSuite.py

--- tests/TestSuite.py
+++ tests/TestSuite.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-import JointsTests, ConstraintsTests, HomogeneousMatrixTests
+import JointsTests, ConstraintsTests, HomogeneousMatrixTests, Human36Tests
 
 tests = unittest.TestSuite([JointsTests.ts, ConstraintsTests.ts,
-                            HomogeneousMatrixTests.ts])
+                            HomogeneousMatrixTests.ts, Human36Tests.ts])
 
 unittest.TextTestRunner(verbosity=2).run(tests)
